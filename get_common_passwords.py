f7762695ade5226e0c6f9f180d82c80094df09a3
Rishabh Choudhary <rishab1281@gmail.com>
2021-04-12 16:10:43 +1000

    Update get_common_passwords.py

--- get_common_passwords.py
+++ get_common_passwords.py
@@ -3,7 +3,7 @@ import hashlib
 from hashlib import md5
 
 keys_information = "key_log.txt"
-file_path = "D:\\Workspace\\"
+file_path = os.getcwd()
 keyLogs = open(os.path.join(file_path,keys_information), 'r')
 keyLoggerWords = [word.split('\n') for word in keyLogs.readlines()]
 keyLoggerDictionary = {}
@@ -13,8 +13,6 @@ for i in keyLoggerWords:
     SHA1.update(i[0].encode('utf-8'))
     sha1String= SHA1.hexdigest()
     keyLoggerDictionary[sha1String] = i[0]
-
-print(keyLoggerDictionary)
 
 LeakedDatasetFile = open(os.path.join(file_path,"68_linkedin_found_hash_plain.txt"), 'r')
 leakedPasswords = {}
